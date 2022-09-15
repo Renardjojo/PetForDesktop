1de6070478f2cb1b19bc68b2a82d564761e96b3c
Six Jonathan <Six-jonathan@orange.fr>
2022-09-15 21:26:42 +0200

    Add template for vec2 (for int) and remove inl

--- include/Vector2.hpp
+++ include/Vector2.hpp
@@ -9,81 +9,339 @@
 #include <cfloat>
 #include <cmath>
 
+template <typename T>
 union Vector2 {
     // Data members
     struct
     {
-        float x;
-        float y;
+        T x;
+        T y;
     };
-    float e[2];
+    T e[2];
 
     Vector2() = default;
-    constexpr Vector2(const float k) noexcept;
-    constexpr Vector2(const float x, const float y = .0f) noexcept;
-    constexpr Vector2(const float coef[2]) noexcept;
+    constexpr Vector2(const T k) noexcept : x{k}, y{k}
+    {
+    }
+
+    constexpr Vector2(const T x_, const T y_ = static_cast<T>(0)) noexcept : x{x_}, y{y_}
+    {
+    }
+
+    constexpr Vector2(const T coef[2]) noexcept : e{coef[0], coef[1]}
+    {
+    }
 
     // Static methods (pseudo-constructors)
-    static constexpr Vector2 zero() noexcept;
-    static constexpr Vector2 one() noexcept;
-    static constexpr Vector2 right() noexcept;
-    static constexpr Vector2 up() noexcept;
-    static constexpr Vector2 left() noexcept;
-    static constexpr Vector2 down() noexcept;
+    static constexpr Vector2 zero() noexcept
+    {
+        return {static_cast<T>(0), static_cast<T>(0)};
+    }
+
+    static constexpr Vector2 one() noexcept
+    {
+        return {static_cast<T>(1), static_cast<T>(1)};
+    }
+
+    static constexpr Vector2 right() noexcept
+    {
+        return {static_cast<T>(1), static_cast<T>(0)};
+    }
+
+    static constexpr Vector2 up() noexcept
+    {
+        return {static_cast<T>(0), static_cast<T>(1)};
+    }
+
+    static constexpr Vector2 left() noexcept
+    {
+        return {-static_cast<T>(1), static_cast<T>(0)};
+    }
+
+    static constexpr Vector2 down() noexcept
+    {
+        return {static_cast<T>(0), -static_cast<T>(1)};
+    }
 
     // Member methods
-    constexpr float   sqrLength() const noexcept;
-    float             length() const noexcept;
-    constexpr float   dot(const Vector2& v) const noexcept;
-    constexpr float   cross(const Vector2& v) const noexcept;
-    constexpr bool    isNull() const noexcept;
-    constexpr bool    isOrthogonalTo(const Vector2& v) const noexcept;
-    constexpr bool    isNormalized() const noexcept;
-    constexpr bool    isOrthonormalTo(const Vector2& v) const noexcept;
-    constexpr bool    isColinearTo(const Vector2& v) const noexcept;
-    bool              isEqualTo(const Vector2& v, const float eps = FLT_EPSILON) const noexcept;
-    bool              isNotEqualTo(const Vector2& v, const float eps = FLT_EPSILON) const noexcept;
-    void              normalize() noexcept;
-    void              safelyNormalize() noexcept;
-    constexpr float   sqrDistanceTo(const Vector2& v) const noexcept;
-    float             distanceTo(const Vector2& v) const noexcept;
-    float             angleWithUnitary(const Vector2& v) const noexcept;
-    float             angleWith(const Vector2& v) const noexcept;
-    constexpr float   triangleArea(const Vector2& v) const noexcept;
-    Vector2           normalized() const noexcept;
-    Vector2           safelyNormalized() const noexcept;
-    constexpr Vector2 projectedOnUnitary(const Vector2& v) const noexcept;
-    constexpr Vector2 projectedOn(const Vector2& v) const noexcept;
-    Vector2           rotated(const float angle) const noexcept;
-    constexpr Vector2 rotated90() const noexcept;
-    constexpr Vector2 lerp(const Vector2& v, const float f) const noexcept;
-    constexpr Vector2 reflect(const Vector2& n) const noexcept;
+    constexpr T sqrLength() const noexcept
+    {
+        return (x * x) + (y * y);
+    }
+
+    T length() const noexcept
+    {
+        return sqrtf(sqrLength());
+    }
+
+    constexpr T dot(const Vector2& v) const noexcept
+    {
+        return (x * v.x) + (y * v.y);
+    }
+
+    constexpr T cross(const Vector2& v) const noexcept
+    {
+        return (x * v.y) - (y * v.x);
+    }
+
+    constexpr bool isNull() const noexcept
+    {
+        return !x && !y;
+    }
+
+    constexpr bool isOrthogonalTo(const Vector2& v) const noexcept
+    {
+        return !dot(v);
+    }
+
+    constexpr bool isNormalized() const noexcept
+    {
+        return !(sqrLength() - static_cast<T>(1));
+    }
+
+    constexpr bool isOrthonormalTo(const Vector2& v) const noexcept
+    {
+        return !dot(v) && isNormalized() && v.isNormalized();
+    }
+
+    constexpr bool isColinearTo(const Vector2& v) const noexcept
+    {
+        return !cross(v);
+    }
+
+    bool isEqualTo(const Vector2& v, const T eps = FLT_EPSILON) const noexcept
+    {
+        return fabs(x - v.x) <= eps && fabs(y - v.y) <= eps;
+    }
+
+    bool isNotEqualTo(const Vector2& v, const T eps = FLT_EPSILON) const noexcept
+    {
+        return fabs(x - v.x) > eps || fabs(y - v.y) > eps;
+    }
+
+    void normalize() noexcept
+    {
+        *this /= length();
+    }
+
+    void safelyNormalize() noexcept
+    {
+        const float sqrLen{sqrLength()};
+
+        if (sqrLen)
+            *this /= sqrtf(sqrLen);
+    }
+
+    T angleWithUnitary(const Vector2& v) const noexcept
+    {
+        return acosf(dot(v) / length());
+    }
+
+    T angleWith(const Vector2& v) const noexcept
+    {
+        return acosf(dot(v) / sqrtf(sqrLength() * v.sqrLength()));
+    }
+
+    constexpr T triangleArea(const Vector2& v) const noexcept
+    {
+        return cross(v) * .5f;
+    }
+
+    Vector2 normalized() const noexcept
+    {
+        return *this / length();
+    }
+
+    Vector2 safelyNormalized() const noexcept
+    {
+        const float sqrLen{sqrLength()};
+
+        return sqrLen ? (*this / sqrtf(sqrLen)) : *this;
+    }
+
+    constexpr Vector2 projectedOnUnitary(const Vector2& v) const noexcept
+    {
+        return v * dot(v);
+    }
+
+    constexpr Vector2 projectedOn(const Vector2& v) const noexcept
+    {
+        return v * (dot(v) / v.sqrLength());
+    }
+
+    Vector2 rotated(const T angle) const noexcept
+    {
+        const float cosAngle{cosf(angle)}, sinAngle{sinf(angle)};
+
+        return {(cosAngle * x) - (sinAngle * y), (sinAngle * x) + (cosAngle * y)};
+    }
+
+    constexpr Vector2 rotated90() const noexcept
+    {
+        return {y, -x};
+    }
+
+    constexpr Vector2 lerp(const Vector2& v, const T t) const noexcept
+    {
+        const float tmp{static_cast<T>(1) - t};
+
+        return {(x * tmp) + (v.x * t), (y * tmp) + (v.y * t)};
+    }
+
+    constexpr Vector2 reflect(const Vector2& n) const noexcept
+    {
+        return *this - n * (2.f * dot(n));
+    }
 
     // Operator overloads
-    constexpr Vector2& operator+=(const Vector2& v) noexcept;
-    constexpr Vector2& operator+=(const Vector2&& v) noexcept;
-    constexpr Vector2& operator-=(const Vector2& v) noexcept;
-    constexpr Vector2& operator-=(const Vector2&& v) noexcept;
-    constexpr Vector2& operator*=(const Vector2& v) noexcept;
-    constexpr Vector2& operator*=(const Vector2&& v) noexcept;
-    constexpr Vector2& operator/=(const Vector2& v) noexcept;
-    constexpr Vector2& operator/=(const Vector2&& v) noexcept;
-    constexpr Vector2& operator*=(const float k) noexcept;
-    constexpr Vector2& operator/=(const float k) noexcept;
-    constexpr Vector2  operator+(const Vector2& v) const noexcept;
-    constexpr Vector2  operator+(const Vector2&& v) const noexcept;
-    constexpr Vector2  operator-(const Vector2& v) const noexcept;
-    constexpr Vector2  operator-(const Vector2&& v) const noexcept;
-    constexpr Vector2  operator*(const Vector2& v) const noexcept;
-    constexpr Vector2  operator*(const Vector2&& v) const noexcept;
-    constexpr Vector2  operator/(const Vector2& v) const noexcept;
-    constexpr Vector2  operator/(const Vector2&& v) const noexcept;
-    constexpr Vector2  operator-() const noexcept;
-    constexpr Vector2  operator*(const float k) const noexcept;
-    constexpr Vector2  operator/(const float k) const noexcept;
+    constexpr Vector2& operator+=(const Vector2& v) noexcept
+    {
+        x += v.x;
+        y += v.y;
+
+        return *this;
+    }
+
+    constexpr Vector2& operator+=(const Vector2&& v) noexcept
+    {
+        x += v.x;
+        y += v.y;
+
+        return *this;
+    }
+
+    constexpr Vector2& operator-=(const Vector2& v) noexcept
+    {
+        x -= v.x;
+        y -= v.y;
+
+        return *this;
+    }
+
+    constexpr Vector2& operator-=(const Vector2&& v) noexcept
+    {
+        x -= v.x;
+        y -= v.y;
+
+        return *this;
+    }
+
+    constexpr Vector2& operator*=(const Vector2& v) noexcept
+    {
+        x *= v.x;
+        y *= v.y;
+
+        return *this;
+    }
+
+    constexpr Vector2& operator*=(const Vector2&& v) noexcept
+    {
+        x *= v.x;
+        y *= v.y;
+
+        return *this;
+    }
+
+    constexpr Vector2& operator/=(const Vector2& v) noexcept
+    {
+        const float reciprocal{static_cast<T>(1) / (v.x * v.y)};
+
+        x *= v.y * reciprocal;
+        y *= v.x * reciprocal;
+
+        return *this;
+    }
+
+    constexpr Vector2& operator/=(const Vector2&& v) noexcept
+    {
+        const float reciprocal{static_cast<T>(1) / (v.x * v.y)};
+        x *= v.y * reciprocal;
+        y *= v.x * reciprocal;
+
+        return *this;
+    }
+
+    constexpr Vector2& operator*=(const T k) noexcept
+    {
+        x *= k;
+        y *= k;
+
+        return *this;
+    }
+
+    constexpr Vector2& operator/=(const T k) noexcept
+    {
+        const float reciprocal{static_cast<T>(1) / k};
+
+        x *= reciprocal;
+        y *= reciprocal;
+
+        return *this;
+    }
+
+    constexpr Vector2 operator+(const Vector2& v) const noexcept
+    {
+        return {x + v.x, y + v.y};
+    }
+
+    constexpr Vector2 operator+(const Vector2&& v) const noexcept
+    {
+        return {x + v.x, y + v.y};
+    }
+
+    constexpr Vector2 operator-(const Vector2& v) const noexcept
+    {
+        return {x - v.x, y - v.y};
+    }
+
+    constexpr Vector2 operator-(const Vector2&& v) const noexcept
+    {
+        return {x - v.x, y - v.y};
+    }
+
+    constexpr Vector2 operator*(const Vector2& v) const noexcept
+    {
+        return {x * v.x, y * v.y};
+    }
+
+    constexpr Vector2 operator*(const Vector2&& v) const noexcept
+    {
+        return {x * v.x, y * v.y};
+    }
+
+    constexpr Vector2 operator/(const Vector2& v) const noexcept
+    {
+        const float reciprocal{static_cast<T>(1) / (v.x * v.y)};
+
+        return {x * v.y * reciprocal, y * v.x * reciprocal};
+    }
+
+    constexpr Vector2 operator/(const Vector2&& v) const noexcept
+    {
+        const float reciprocal{static_cast<T>(1) / (v.x * v.y)};
+
+        return {x * v.y * reciprocal, y * v.x * reciprocal};
+    }
+
+    constexpr Vector2 operator-() const noexcept
+    {
+        return {-x, -y};
+    }
+
+    constexpr Vector2 operator*(const T k) const noexcept
+    {
+        return {x * k, y * k};
+    }
+
+    constexpr Vector2 operator/(const T k) const noexcept
+    {
+        const float reciprocal{static_cast<T>(1) / k};
+
+        return {x * reciprocal, y * reciprocal};
+    }
 };
 
-using Vec2 = Vector2;
-using vec2 = Vector2;
-
-#include "Vector2.inl"
+using Vec2  = Vector2<float>;
+using vec2  = Vector2<float>;
+using Vec2i = Vector2<int>;
+using vec2i = Vector2<int>;
