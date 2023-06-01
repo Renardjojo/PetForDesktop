6288f752ec3c8061e53fc27e02827401f189587f
Six Jonathan <Six-jonathan@orange.fr>
2023-06-01 16:35:24 +0200

    Fix OGL shaders

--- content/shader/glsl/dFdxEdgeDetection.fs
+++ content/shader/glsl/dFdxEdgeDetection.fs
@@ -5,11 +5,8 @@ layout (location = 0) out float FragColor;
 layout (location = 0) in vec2 TexCoord;
 
 // texture samplers
-layout (binding = 0) uniform sampler2D uTexture;
-layout (binding = 1) uniform TextureResolution
-{
-  vec2 resolution;
-} resolution;
+uniform sampler2D uTexture;
+uniform vec2 uResolution;
 
 void main()
 {
@@ -19,7 +16,7 @@ void main()
     //FragColor = float(diffY > 0.05);
 
     vec3 col = texture(uTexture, TexCoord).rgb;
-    vec2 uvColY = vec2(TexCoord.x, TexCoord.y + 1.0 / resolution.resolution.y);
+    vec2 uvColY = vec2(TexCoord.x, TexCoord.y + 1.0 / uResolution.y);
     vec3 colY = texture(uTexture, uvColY).rgb;
     vec3 dy = colY - col;
     float diffY = (abs(dy.x) + abs(dy.y) + abs(dy.z)) / 3.0;
--- content/shader/glsl/spriteSheet.vs
+++ content/shader/glsl/spriteSheet.vs
@@ -3,14 +3,11 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec2 aTexCoord;
 
 layout (location = 0) out vec2 TexCoord;
-layout (binding = 0) uniform ScreenParam
-{
-  vec4 uClipSpacePosSize;
-  vec4 uScaleOffSet;
-} screenParam;
+uniform vec4 uClipSpacePosSize;
+uniform vec4 uScaleOffSet;
 
 void main()
 {
-	gl_Position = vec4((aPos.xy * screenParam.uClipSpacePosSize.zw + screenParam.uClipSpacePosSize.xy) * 2.0 - 1.0, 1.0, 1.0);
-	TexCoord = vec2(aTexCoord.x, aTexCoord.y) * screenParam.uScaleOffSet.xy + screenParam.uScaleOffSet.zw;
+	gl_Position = vec4((aPos.xy * uClipSpacePosSize.zw + uClipSpacePosSize.xy) * 2.0 - 1.0, 1.0, 1.0);
+	TexCoord = vec2(aTexCoord.x, aTexCoord.y) * uScaleOffSet.xy + uScaleOffSet.zw;
 }
