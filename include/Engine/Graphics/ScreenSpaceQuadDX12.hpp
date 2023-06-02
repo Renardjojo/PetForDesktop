c7a70bc24200dd0883e4a95205edcaa56c6e5528
Six Jonathan <Six-jonathan@orange.fr>
2023-06-02 12:49:44 +0200

    implemente use and drax for mesh

--- include/Engine/Graphics/ScreenSpaceQuadDX12.hpp
+++ include/Engine/Graphics/ScreenSpaceQuadDX12.hpp
@@ -2,6 +2,7 @@
 
 #include "Engine/Graphics/WindowDX12.hpp"
 #include "Engine/Log.hpp"
+#include "Engine/Graphics/WindowDX12.hpp"
 #include "d3dx12/d3dx12.h"
 #include <d3d12.h> // for D3D12 interface
 #include <wrl.h>   // for Microsoft::WRL::ComPtr
@@ -15,13 +16,17 @@ class ScreenSpaceQuad
 {
 protected:
     // GPU memory for vertex data
-    ComPtr<ID3D12Resource> m_vertexBuffer;
+    D3D12_VERTEX_BUFFER_VIEW m_vertexBufferView;
+    ComPtr<ID3D12Resource>   m_vertexBuffer;
 
     // GPU memory for index data
-    ComPtr<ID3D12Resource> m_indexBuffer;
+    D3D12_INDEX_BUFFER_VIEW m_indexBufferView;
+    ComPtr<ID3D12Resource>  m_indexBuffer;
+
+    Window& m_window;
 
 public:
-    ScreenSpaceQuad(Window& win, float minPos = -1.f, float maxPos = 1.f)
+    ScreenSpaceQuad(Window& win, float minPos = -1.f, float maxPos = 1.f) : m_window(win)
     {
         // TODO: Pos vec2 ?
         const float vBuffer[] = {
@@ -32,7 +37,7 @@ public:
             minPos, maxPos, 0.0f, 0.0f, 1.0f  // top left
         };
 
-        createVertexBuffer(win, minPos, maxPos, vBuffer);
+        createVertexBuffer(win, minPos, maxPos, vBuffer, sizeof(float) * 5);
 
         const unsigned int iBuffer[] = {
             0, 1, 3, // first triangle
@@ -42,7 +47,7 @@ public:
         createIndexBuffer(win, iBuffer);
     }
 
-    HRESULT createVertexBuffer(Window& win, float minPos, float maxPos, const float* vBuffer)
+    void createVertexBuffer(Window& win, float minPos, float maxPos, const float* vBuffer, unsigned int vertexSize)
     {
         HRESULT hr;
 
@@ -54,13 +59,13 @@ public:
         // an upload heap
         CD3DX12_HEAP_PROPERTIES heapProperties(D3D12_HEAP_TYPE_DEFAULT);
         CD3DX12_RESOURCE_DESC   buffer = CD3DX12_RESOURCE_DESC::Buffer(vBufferSize);
-        V_RETURN(win.getDevice()->CreateCommittedResource(
+        V(win.getDevice()->CreateCommittedResource(
             &heapProperties, D3D12_HEAP_FLAG_NONE, &buffer,
             D3D12_RESOURCE_STATE_COPY_DEST, // we will start this heap in the copy destination state since we will copy
                                             // data from the upload heap to this heap
             nullptr, IID_PPV_ARGS(&m_vertexBuffer)))
 
-        V_RETURN(m_vertexBuffer->SetName(L"Vertex Buffer Resource Heap"))
+        V(m_vertexBuffer->SetName(L"Vertex Buffer Resource Heap"))
 
         // create upload heap
         // upload heaps are used to upload data to the GPU. CPU can write to it, GPU can read from it
@@ -68,12 +73,12 @@ public:
         ComPtr<ID3D12Resource> vBufferUploadHeap;
         heapProperties = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
         buffer         = CD3DX12_RESOURCE_DESC::Buffer(vBufferSize);
-        V_RETURN(win.getDevice()->CreateCommittedResource(
+        V(win.getDevice()->CreateCommittedResource(
             &heapProperties, D3D12_HEAP_FLAG_NONE, &buffer,
             D3D12_RESOURCE_STATE_GENERIC_READ, // GPU will read from this buffer and copy its contents to the default
                                                // heap
             nullptr, IID_PPV_ARGS(&vBufferUploadHeap)))
-        V_RETURN(vBufferUploadHeap->SetName(L"Vertex Buffer Upload Resource Heap"))
+        V(vBufferUploadHeap->SetName(L"Vertex Buffer Upload Resource Heap"))
 
         // store vertex buffer in upload heap
         D3D12_SUBRESOURCE_DATA vertexData = {};
@@ -90,7 +95,11 @@ public:
             m_vertexBuffer.Get(), D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER);
         win.getCommandList()->ResourceBarrier(1, &barrier);
 
-        return 0;
+        // create a vertex buffer view for the triangle. We get the GPU memory address to the vertex pointer using the
+        // GetGPUVirtualAddress() method
+        m_vertexBufferView.BufferLocation = m_vertexBuffer->GetGPUVirtualAddress();
+        m_vertexBufferView.StrideInBytes  = vertexSize;
+        m_vertexBufferView.SizeInBytes    = vBufferSize;
     }
 
     void createIndexBuffer(Window& win, const unsigned int* iBuffer)
@@ -115,12 +124,12 @@ public:
         ComPtr<ID3D12Resource> iBufferUploadHeap;
         heapProperties = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
         buffer         = CD3DX12_RESOURCE_DESC::Buffer(iBufferSize);
-        V_RETURN(win.getDevice()->CreateCommittedResource(
+        V(win.getDevice()->CreateCommittedResource(
             &heapProperties, D3D12_HEAP_FLAG_NONE, &buffer,
             D3D12_RESOURCE_STATE_GENERIC_READ, // GPU will read from this buffer and copy its contents to the default
                                                // heap
             nullptr, IID_PPV_ARGS(&iBufferUploadHeap)))
-        V_RETURN(iBufferUploadHeap->SetName(L"Index Buffer Upload Resource Heap"))
+        V(iBufferUploadHeap->SetName(L"Index Buffer Upload Resource Heap"))
 
         // store vertex buffer in upload heap
         D3D12_SUBRESOURCE_DATA indexData = {};
@@ -137,7 +146,12 @@ public:
             m_indexBuffer.Get(), D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER);
         win.getCommandList()->ResourceBarrier(1, &barrier);
 
-        return 0;
+        // create a vertex buffer view for the triangle. We get the GPU memory address to the vertex pointer using the
+        // GetGPUVirtualAddress() method
+        m_indexBufferView.BufferLocation = m_indexBuffer->GetGPUVirtualAddress();
+        m_indexBufferView.Format =
+            DXGI_FORMAT_R32_UINT; // 32-bit unsigned integer (this is what a dword is, double word, a word is 2 bytes)
+        m_indexBufferView.SizeInBytes = iBufferSize;
     }
 
     ~ScreenSpaceQuad()
@@ -146,9 +160,12 @@ public:
 
     void use()
     {
+        m_window.getCommandList()->IASetVertexBuffers(0, 1, &m_vertexBufferView); // set the vertex buffer (using the vertex buffer view)
+        m_window.getCommandList()->IASetIndexBuffer(&m_indexBufferView);
     }
 
     void draw()
     {
+        m_window.getCommandList()->DrawIndexedInstanced(6, 1, 0, 0, 0);
     }
 };
